72d886924a88e094d195e3adc6c14c1dcd872fb1
Theera12 <aarthy.maaya@gmail.com>
2025-12-21 01:08:07 -0800

    Controlled Components & Forms

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,28 @@ import TodoForm from './TodoForm';
 import { useState } from 'react';
 function App() {
   const [todoList, setTodoList] = useState([]);
+
   function addTodo(title) {
     //New object is constructed to hold the all values entered in input form
-    const newTodo = { id: Date.now(), title: title };
+    const newTodo = { id: Date.now(), title: title, isCompleted: false };
     setTodoList([...todoList, newTodo]);
+  }
+
+  function completeTodo(id) {
+    //creating checkbox to specify all the completed task
+    const updatedTodos = todoList.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, isCompleted: true };
+      }
+      return todo;
+    });
+    setTodoList(updatedTodos);
   }
   return (
     <div>
       <h1>MY TODOS</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
     </div>
   );
 }
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,14 +1,26 @@
 import './App.css';
 import TodoListItem from './TodoListItem.jsx';
 
-function TodoList({ todoList }) {
+function TodoList({ todoList, onCompleteTodo }) {
+  //filter out the todos if Completed
+  const filteredTodoList = todoList.filter((todo) => !todo.isCompleted);
   return (
     <>
-      <ul>
-        {todoList.map((todo) => (
-          <TodoListItem key={todo.id} todo={todo} />
-        ))}
-      </ul>
+      {filteredTodoList.length === 0 ? (
+        <p>Add Todo to get Started...</p>
+      ) : (
+        <ul>
+          {filteredTodoList.map((todo) => (
+            <TodoListItem
+              key={todo.id}
+              todo={todo}
+              onCompleteTodo={() => {
+                onCompleteTodo(todo.id);
+              }}
+            />
+          ))}
+        </ul>
+      )}
     </>
   );
 }
